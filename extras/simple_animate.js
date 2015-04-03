604e426632a90bb7ce060ff83a28539bf0d3c7f2
Filip Defar <filip.defar@gmail.com>
2015-04-03 16:03:21 +0200

    Fix animations

--- extras/simple_animate.js
+++ extras/simple_animate.js
@@ -1,5 +1,6 @@
 SimpleAnimate = function(target, animationName, callback) {
   $(target).removeClass('hide');
+  $(target).removeClass('animated bounce flash pulse rubberBand shake swing tada wobble bounceIn bounceInDown bounceInLeft bounceInRight bounceInUp bounceOut bounceOutDown bounceOutLeft bounceOutRight bounceOutUp fadeIn fadeInDown fadeInDownBig fadeInLeft fadeInLeftBig fadeInRight fadeInRightBig fadeInUp fadeInUpBig fadeOut fadeOutDown fadeOutDownBig fadeOutLeft fadeOutLeftBig fadeOutRight fadeOutRightBig fadeOutUp fadeOutUpBig flipInX flipInY flipOutX flipOutY lightSpeedIn lightSpeedOut rotateIn rotateInDownLeft rotateInDownRight rotateInUpLeft rotateInUpRight rotateOut rotateOutDownLeft rotateOutDownRight rotateOutUpLeft rotateOutUpRight hinge rollIn rollOut zoomIn zoomInDown zoomInLeft zoomInRight zoomInUp zoomOut zoomOutDown zoomOutLeft zoomOutRight zoomOutUp slideInDown slideInLeft slideInRight slideInUp slideOutDown slideOutLeft slideOutRight slideOutUp');
   $(target).addClass('animated ' + animationName);
   $(target).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', callback);
 }
